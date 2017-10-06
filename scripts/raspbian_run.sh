43e08f42389ed61daf13e2e0c14210199a61ddbe
Regis <boudinot.regis@yahoo.com>
2017-10-05 19:17:57 -0600

    explain about reverse proxy usage

--- scripts/raspbian_run.sh
+++ scripts/raspbian_run.sh
@@ -6,4 +6,8 @@
 # find the PID for the non screen process
 # should be the second one in the list!
 
+# this will run on localhost:8080
+# use a reverse proxy (caddy-server/nginx/apache)
+# to proxy the request to the raspberry pi 
+
 screen -d -m ./builds/rpi_main
