82a88f9764e507b97573186d6ae929ca2842504a
Regis <boudinot.regis@yahoo.com>
2017-10-07 02:16:19 -0600

    fun lil benchmarking

---
+++ scripts/bench.sh
@@ -0,0 +1,7 @@
+# this will run a concurrent benchmark on the server
+# create a file called results (with the results)
+# and then checkout the file from git to not pollute git history
+cd ab_bench \
+  && ./bench.sh \
+  && cd .. \
+  && git checkout results
