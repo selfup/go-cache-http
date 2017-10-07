4a011e28ffcba2e5bb98e3966e78456b4a3938a8
Regis <boudinot.regis@yahoo.com>
2017-10-07 04:54:34 -0600

    folder structure

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,7 +1,11 @@
 # this will run a concurrent benchmark on the server
 # create a file called results (with the results)
 # and then checkout the file from git to not pollute git history
-cd ab_bench \
-  && ./bench.sh \
-  && cd .. \
+ab \
+  -n 100000 \
+  -c 10 \
+  -k -v 1 \
+  -H "Accept-Encoding: gzip, deflate" \
+  -T "application/json" \
+  -p ./scripts/test.txt http://localhost:8080/ > .results \
   && git checkout .results
