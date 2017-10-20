bf39bf5c79d3809275685fa630d2d1f852d67959
Regis <boudinot.regis@yahoo.com>
2017-10-20 04:36:00 -0600

    don't test main - move all testable logic out of main

---
+++ handlers/defineport.go
@@ -0,0 +1,14 @@
+package handlers
+
+import "os"
+
+// DefinePort either grabs the PORT ENV or defines 8080 as the port
+func DefinePort() string {
+	portEnv := os.Getenv("PORT")
+
+	if portEnv != "" {
+		return ":" + portEnv
+	}
+
+	return ":8080"
+}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"os"
 
 	"github.com/selfup/gocrashttp/handlers"
 	"github.com/selfup/gocrashttp/lib"
@@ -12,18 +11,7 @@ var (
 	state = make(map[string]*lib.CacheData)
 )
 
-// DefinePort either grabs the PORT ENV or defines 8080 as the port
-func DefinePort() string {
-	portEnv := os.Getenv("PORT")
-
-	if portEnv != "" {
-		return ":" + portEnv
-	}
-
-	return ":8080"
-}
-
 func main() {
 	http.HandleFunc("/", handlers.FetchCacheOrUpdate(state))
-	http.ListenAndServe(DefinePort(), nil)
+	http.ListenAndServe(handlers.DefinePort(), nil)
 }
