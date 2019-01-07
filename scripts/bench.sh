1aa570f695b055894d8954278875ef01eeba884f
selfup <boudinot.regis@yahoo.com>
2019-01-07 00:10:46 -0600

    update bench script and results

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -5,12 +5,19 @@ else
   APP_HOST=localhost
 fi
 
+if [ "$2" == "" ]
+then
+  CONCURRENT_CALLS=300
+  RESULTS_FILE=.con300.results.log
+else
+  CONCURRENT_CALLS=$2
+  RESULTS_FILE=.con$2.results.log
+fi
+
 ab \
   -n 50000 \
-  -c 300 \
+  -c $CONCURRENT_CALLS \
   -k -v 1 \
   -H "Accept-Encoding: gzip, deflate" \
   -T "application/json" \
-  -p ./scripts/bench.txt "http://$APP_HOST:8081/" > .results.log \
-
-git checkout -- .results.log
+  -p ./scripts/bench.txt "http://$APP_HOST:8081/" > $RESULTS_FILE \
